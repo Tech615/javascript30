239aecfc32c14e3ddd502007cf8e45c4acf91ca1
James Leslie Miller <silverback@gorillabunny.com>
2016-12-09 22:07:16 -0600

    Add project 01 javascript file for jlm

---
+++ 01 - JavaScript Drum Kit/james.js
@@ -0,0 +1,24 @@
+function removeTransition(e) {
+    if ('transform' !== e.propertyName) return;
+
+    this.classList.remove('playing');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return;
+
+    audio.currentTime = 0;
+
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
