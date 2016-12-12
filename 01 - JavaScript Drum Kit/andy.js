749252eb3fe6ab0eb5a0a438d36e470be09ea3c9
andy matthews <amatthews@eventbrite.com>
2016-12-12 09:33:53 -0600

    Use template literal interpolation

--- 01 - JavaScript Drum Kit/andy.js
+++ 01 - JavaScript Drum Kit/andy.js
@@ -3,10 +3,9 @@ var codes = [65, 68, 70, 71, 72, 74, 75, 76, 83];
 document.addEventListener('keydown', function(e){
     // only allowed key presses please
     if (codes.indexOf(e.keyCode) >= 0) {
-        var code = e.keyCode;
         // get a reference to the button, and the audioclip
-        document.querySelector('[data-key="' + code + '"]').classList.add('playing');
-        var audio = document.querySelector('audio[data-key="' + code + '"]');
+        document.querySelector(`[data-key="${e.keyCode}"]`).classList.add('playing');
+        var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
         audio.currentTime = 0;
         audio.play();
     }
@@ -15,8 +14,7 @@ document.addEventListener('keydown', function(e){
 document.addEventListener('keyup', function(e){
     // only allowed key presses please
     if (codes.indexOf(e.keyCode) >= 0) {
-        var code = e.keyCode;
         // get a reference to the button, and the audioclip
-        document.querySelector('[data-key="' + code + '"]').classList.remove('playing');
+        document.querySelector(`[data-key="${e.keyCode}"]`).classList.remove('playing');
     }
 });
